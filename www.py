cb5f0aad030208a4cd30c4db8ae5572d00264b32
fangweiren <fwr843@163.com>
2019-06-18 17:46:11 +0800

    5-2 管理后台:美餐管理、会员列表、财务管理、统计管理

--- www.py
+++ www.py
@@ -5,8 +5,16 @@ from webs.controllers.index import route_index
 from webs.controllers.static import route_static
 from webs.controllers.user.User import route_user
 from webs.controllers.account.Account import route_account
+from webs.controllers.finance.Finance import route_finance
+from webs.controllers.food.Food import route_food
+from webs.controllers.member.Member import route_member
+from webs.controllers.stat.Stat import route_stat
 
 app.register_blueprint(route_index, url_prefix="/")
 app.register_blueprint(route_static, url_prefix="/static")
 app.register_blueprint(route_user, url_prefix="/user")
 app.register_blueprint(route_account, url_prefix="/account")
+app.register_blueprint(route_finance, url_prefix="/finance")
+app.register_blueprint(route_food, url_prefix="/food")
+app.register_blueprint(route_member, url_prefix="/member")
+app.register_blueprint(route_stat, url_prefix="/stat")
