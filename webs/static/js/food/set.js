ece7cb273f5913ed36322616a75e28839262d4c3
fangweiren <fwr843@163.com>
2019-07-12 17:01:48 +0800

    10-3 可视化编辑器ueditor配置

---
+++ webs/static/js/food/set.js
@@ -0,0 +1,35 @@
+;
+var food_set_ops = {
+    init: function () {
+        this.eventBind();
+        this.initEditor();
+    },
+    eventBind: function () {
+
+    },
+    initEditor: function () {
+        var that = this;
+        that.ue = UE.getEditor('editor', {
+            toolbars: [
+                ['undo', 'redo', '|',
+                    'bold', 'italic', 'underline', 'strikethrough', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', '|', 'rowspacingtop', 'rowspacingbottom', 'lineheight'],
+                ['customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+                    'directionalityltr', 'directionalityrtl', 'indent', '|',
+                    'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+                    'link', 'unlink'],
+                ['imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+                    'insertimage', 'insertvideo', '|',
+                    'horizontal', 'spechars', '|', 'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols']
+            ],
+            enableAutoSave: true,
+            saveInterval: 60000,
+            elementPathEnabled: false,
+            zIndex: 4,
+            serverUrl: common_ops.buildUrl('/upload/ueditor')
+        });
+    }
+};
+
+$(document).ready(function () {
+    food_set_ops.init();
+});
