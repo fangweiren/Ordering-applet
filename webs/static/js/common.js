184c4e1c0d81e9369ca0f3f8a66a7f06851c107d
fangweiren <fwr843@163.com>
2019-07-16 13:18:35 +0800

    10-10 小优化和重点回顾

--- webs/static/js/common.js
+++ webs/static/js/common.js
@@ -158,7 +158,9 @@ var common_ops = {
         return url + _paramUrl
     },
     buildPicUrl: function (img_key) {
-        return "http://192.168.37.133:9000/static/upload/" + img_key
+        var domain = $(".hidden_layout_wrap input[name=domain]").val();
+        var prefix_url = $(".hidden_layout_wrap input[name=prefix_url]").val();
+        return domain + prefix_url + img_key
     }
 };
 
